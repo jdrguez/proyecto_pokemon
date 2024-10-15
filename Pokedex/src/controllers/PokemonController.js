5e82eae78465c5d0a8937a11aecdabcd3e839d3a
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-10-15 18:38:35 +0100

    Fix structure

--- Pokedex/src/controllers/PokemonController.js
+++ Pokedex/src/controllers/PokemonController.js
@@ -1,18 +1,26 @@
 // PokemonController.js
+import ConectToFirebase from "../models/conectToFireStore.js";
 import { PokemonModel } from "../models/PokemonModel.js";
 import { PokemonView } from "../views/PokemonView.js";
 export class PokemonController {
   constructor() {
+    this.db = new ConectToFirebase();
     this.model = new PokemonModel();
     this.view = new PokemonView();
 
     this.pokemonsFiltered = [];
     this.newDesireList = [];
+    this.auxPokemons = [];
 
     // Bind button event
     document
       .querySelector("button")
       .addEventListener("click", () => this.init());
+
+    // Bind Botons de BBDD de prueba
+    document.querySelectorAll(".btnBBDD").forEach((btnBBDD) => {
+      btnBBDD.addEventListener("click", () => this.bbddAction(btnBBDD.id));
+    });
   }
   async init() {
     this.view.showLoading();
@@ -29,7 +37,15 @@ export class PokemonController {
   async bindingEvents() {
     // Bind input filterType
     this.filterType = document.querySelector("#filtroTipo");
-    this.filterType.addEventListener("keyup", () => this.filteringPokemons());
+    this.filterType.addEventListener("keyup", () =>
+      this.filteringPokemonsByType()
+    );
+
+    // Bind input filterWeight
+    this.filterWeight = document.querySelector("#filtroPeso");
+    this.filterWeight.addEventListener("keyup", () =>
+      this.filteringPokemonsByWeight()
+    );
 
     // Bind Añadir a Lista de deseos
     document
@@ -43,13 +59,60 @@ export class PokemonController {
     });
   }
 
+  // Metódo de prueba de BBDD
+  bbddAction(btnClicked) {
+    switch (btnClicked) {
+      case "readAllPokemon":
+        this.getAllPokemon();
+        break;
+
+      case "addPokemon":
+        let data = {
+          tipo: "Iron",
+          nombre: "Fixy",
+        };
+        this.createPokemon(data);
+        break;
+
+      case "updatePokemon":
+        const id = "dJEvAx4dTIUY9IOeoy7E";
+        let data2 = {
+          tipo: "Water",
+          nombre: "Darum",
+        };
+        this.updatePokemon(id, data2);
+        break;
+
+      case "deletePokemon":
+        const id2 = "PezyN0gwr0vzXhBApCxU";
+        this.deletePokemon(id2);
+        break;
+
+      default:
+        break;
+    }
+  }
+
   pokemonsClicked(cardId) {
     this.newDesireList.push(cardId);
   }
 
-  async filteringPokemons() {
-    this.pokemonsFiltered = [];
-    this.model.pokemons.forEach((pkm) => {
+  async filteringPokemonsByType() {
+    // Probando BBDD
+    const getTasks = () => getDocs(collection(db, "pokemon"));
+    console.log(`Pokemon de FireStore: ${getTasks}`);
+
+    if (this.pokemonsFiltered.length == 0) {
+      this.auxPokemons = this.model.pokemons;
+    } else {
+      this.auxPokemons = this.pokemonsFiltered;
+    }
+
+    // ToDo refactorizar para reutilizar
+    // método para todos los filtros -> pasar tipo
+    // filtro por referencia
+
+    this.auxPokemons.forEach((pkm) => {
       this.safePokemon = false;
 
       if (pkm.pkm_type[0].type.name.includes(this.filterType.value)) {
@@ -68,6 +131,13 @@ export class PokemonController {
   }
 
   mostrarListaDeseo() {
+    let data = {
+      tipo: "Watwe",
+      nombre: "Darum",
+    };
+
+    //this.getAllPokemon();
+    //this.createPokemon(data);
     //console.log(this.newDesireList);
     let txt = "¿Quieres añadir los siguientes Pokemons a la Lista de Deseo?";
     this.newDesireList.forEach((pkm) => {
@@ -82,5 +152,21 @@ export class PokemonController {
 
       this.newDesireList = [];
     }
+  }
+
+  async createPokemon(data) {
+    return await this.db.create(data);
+  }
+
+  async getAllPokemon() {
+    return await this.db.readAll();
+  }
+
+  async updatePokemon(id, data) {
+    return await this.db.update(id, data);
+  }
+
+  async deletePokemon(id) {
+    return await this.db.delete(id);
   }
 }
---
+++ Pokedex/src/models/conectToFireStore.js
@@ -0,0 +1,82 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDIYufbRXBkkMtDdm172waEE0yJ4VWJZGM",
+  authDomain: "compra-pokemon.firebaseapp.com",
+  projectId: "compra-pokemon",
+  storageBucket: "compra-pokemon.appspot.com",
+  messagingSenderId: "307179579293",
+  appId: "1:307179579293:web:3cda9bbff5b9371e8db174",
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Firestore
+const db = getFirestore(app);
+
+class ConectToFirebase {
+  constructor() {
+    this.collectionRef = collection(db, "pokemon");
+  }
+
+  // Crear un nuevo documento
+  async create(data) {
+    try {
+      const docRef = await addDoc(this.collectionRef, data);
+      console.log("Documento escrito con ID: ", docRef.id);
+      return docRef.id;
+    } catch (e) {
+      console.error("Error añadiendo documento: ", e);
+    }
+  }
+
+  // Leer todos los documentos
+  async readAll() {
+    try {
+      const querySnapshot = await getDocs(this.collectionRef);
+      const dataList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log("Documentos:", dataList);
+      return dataList;
+    } catch (e) {
+      console.error("Error obteniendo documentos: ", e);
+    }
+  }
+
+  // Actualizar un documento por ID
+  async update(id, data) {
+    try {
+      const docRef = doc(this.collectionRef, id);
+      await updateDoc(docRef, data);
+      console.log("Documento actualizado con ID: ", id);
+    } catch (e) {
+      console.error("Error actualizando documento: ", e);
+    }
+  }
+
+  // Eliminar un documento por ID
+  async delete(id) {
+    try {
+      const docRef = doc(this.collectionRef, id);
+      await deleteDoc(docRef);
+      console.log("Documento eliminado con ID: ", id);
+    } catch (e) {
+      console.error("Error eliminando documento: ", e);
+    }
+  }
+}
+
+export default ConectToFirebase;
