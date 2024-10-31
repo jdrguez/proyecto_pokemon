32b2b78820d6a5a00ebf0ec32d96d9caad284c01
jrguez <josedomingo.rguez.rguez@gmail.com>
2024-10-31 16:03:57 +0000

    fix erros

--- Pokedex/src/models/Pokemon.js
+++ Pokedex/src/models/Pokemon.js
@@ -4,11 +4,19 @@ export class Pokemon {
   constructor(data) {
     this.name = data.name; // Nombre del pokemon
     this.id = data.id; // Id del pokemon
-    this.attack = data.stats[1].base_stat;
+    this.attack = this.getAttackFromStats(data);
     this.price = 0;
-    this.weight = data.weight; // Pokemon peso
-    this.pkm_front = data.sprites.front_default; // Pokemon de frente
-    this.pkm_back = data.sprites.back_default; // Pokemon de espaldas
+    this.weight = 0; // Pokemon peso
+    this.pkm_front = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    this.pkm_back =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`;
     this.pkm_type = data.types; // Tipo del pokemon (Devuelve un array)
   }
+
+  getAttackFromStats(stats) {
+    if (!stats || !Array.isArray(stats)) return 0;
+    const attackStat = stats.find(stat => stat.stat.name === 'attack');
+    return attackStat ? attackStat.base_stat : 0;
+  }
+
+
 }
--- Pokedex/src/script.js
+++ Pokedex/src/script.js
@@ -3,4 +3,11 @@ import { PokemonController } from "./controllers/PokemonController.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const controller = new PokemonController();
+
+
+  document.querySelector("#button").addEventListener("click", () => {
+        controller.init();
+    });
+
+
 });
